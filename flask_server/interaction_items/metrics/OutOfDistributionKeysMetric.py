b7146e3175c5b48a0da2bae61f62f9b9a46079af
eric <eric-m.benz@arcor.de>
2025-09-24 12:39:42 +0200

    Fix: Readability of Data Descriptors

--- flask_server/interaction_items/metrics/OutOfDistributionKeysMetric.py
+++ flask_server/interaction_items/metrics/OutOfDistributionKeysMetric.py
@@ -55,7 +55,7 @@ class OutOfDistributionKeysMetric(MetricItem):
                 )
 
                 subject = request['subject']
-                datapoint_name = f'Layer {rome_module.layer} | Subject "{subject}"'
+                datapoint_name = f'{rome_module.get_name()} | Layer {rome_module.layer} | Subject "{subject}"'
                 metric_values[datapoint_name] = torch.dot(cur_input, left_vector).item()
 
         return metric_values
