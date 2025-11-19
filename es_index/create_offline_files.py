45d44b2b5efe21b2b2648334571bd9c2a880b068
eric <eric-m.benz@arcor.de>
2025-11-19 19:10:29 +0100

    es_index update to various models

--- es_index/create_offline_files.py
+++ es_index/create_offline_files.py
@@ -9,37 +9,56 @@ import numpy as np
 import pyhocon
 import torch
 from tqdm import tqdm
-from transformers import LlamaForCausalLM, CodeLlamaTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 warnings.filterwarnings('ignore')
 
 
-def get_all_projected_values(model):
+def get_all_projected_values(model, config, modules_dict):
     logits = []
+    embedding_weights = modules_dict[config.layer_mappings["token_embedding"]].weight
     for i in tqdm(range(model.config.num_hidden_layers)):
-        layer_logits = torch.matmul(model.model.embed_tokens.weight, model.model.layers[i].mlp.down_proj.weight).T
+        mlp_down_weights = modules_dict[config.layer_mappings["mlp_down_proj"].format(i)].weight
+        # layer_logits = torch.matmul(model.model.embed_tokens.weight, model.model.layers[i].mlp.down_proj.weight).T
+        if "llama" in config.model_name:
+            layer_logits = torch.matmul(embedding_weights, mlp_down_weights).T
+        elif "gpt" in config.model_name:
+            layer_logits = torch.matmul(embedding_weights, mlp_down_weights.T).T
+        else:
+            raise ValueError(f"Model nor recognized {config.model_name}")
         logits.append(layer_logits.detach().cpu())
 
+    print("stacking weights...")
     logits = torch.vstack(logits)
     return logits.numpy()
 
 
-def create_elastic_search_data(path, model, model_name, tokenizer, top_k):
+def create_elastic_search_data(path, model, tokenizer, config, top_k):
     if os.path.isfile(path):
         with open(path, 'rb') as handle:
             dict_es = pickle.load(handle)
             return dict_es
+    if not os.path.isdir(config.server_files_dir):
+        os.mkdir(config.server_files_dir)
     d = {}
     inv_d = {}
     cnt = 0
-    total_dims = model.model.layers[0].mlp.down_proj.weight.size(1)
-    for i in range(model.config.num_hidden_layers):
-        for j in range(total_dims):
+    modules_dict = dict(model.named_modules())
+    if "llama" in config.model_name:
+        total_dims = modules_dict[config.layer_mappings["mlp_down_proj"].format(0)].weight.size(1)
+    elif "gpt" in config.model_name:
+        total_dims = modules_dict[config.layer_mappings["mlp_down_proj"].format(0)].weight.size(0)
+    else:
+        raise ValueError(f"Model nor recognized {config.model_name}")
+    #total_dims = modules_dict[config.layer_mappings["mlp_down_proj"].format(0)].weight.size(1)
+    #total_dims = model.model.layers[0].mlp.down_proj.weight.size(1)
+    for i in tqdm(range(model.config.num_hidden_layers)):
+        for j in tqdm(range(total_dims), leave=False):
             d[cnt] = (i, j)
             inv_d[(i, j)] = cnt
             cnt += 1
     dict_es = {}
-    logits = get_all_projected_values(model)
+    logits = get_all_projected_values(model, config, modules_dict)
     for i in tqdm(range(model.config.num_hidden_layers)):
         for j in tqdm(range(total_dims), leave=False):
             k = (i, j)
@@ -60,9 +79,8 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
     config = pyhocon.ConfigFactory.from_dict(json.loads(_jsonnet.evaluate_file(args.config_path)))
-    model = LlamaForCausalLM.from_pretrained(config.model_name, torch_dtype=torch.float16)
+    model = AutoModelForCausalLM.from_pretrained(config.model_name, torch_dtype=torch.float16)
     device = config.device
     model.to(device)
-    tokenizer = CodeLlamaTokenizer.from_pretrained(config.model_name)
-    _ = create_elastic_search_data(config.elastic_projections_path, model, config.model_name, tokenizer,
-                                   config.top_k_for_elastic)
+    tokenizer = AutoTokenizer.from_pretrained(config.model_name)
+    _ = create_elastic_search_data(config.elastic_projections_path, model, tokenizer, config, config.top_k_for_elastic)
--- streamlit/exploration.py
+++ streamlit/exploration.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 # Parse Arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--config_path", type=str, default="../config_files/codellama-7b-instruct.jsonnet")
+parser.add_argument("--config_path", type=str, default="config_files/codellama-7b-instruct.jsonnet")
 args = parser.parse_args()
 json_config = args.config_path
 config = json.loads(_jsonnet.evaluate_file(json_config))
@@ -28,19 +28,6 @@ with Elasticsearch(
     search_indices = [config["elastic_index"]]
     search_indices_types = ["LMDebuggerIntervention"]
 
-    for sae_path in config['sae_paths']:
-        with open(sae_path, "rb") as f:
-            sae_config = pickle.load(f)
-
-        # ElasticSearch Index-Name
-        index_name = (f'{sae_config["MODEL_TYPE"]}_{sae_config["LAYER_TYPE"]}_{sae_config["LAYER_INDEX"]}_'
-                      f'{sae_config["ACT_VEC_SIZE"]}_{sae_config["DICT_VEC_SIZE"]}_{sae_config["LEARNING_RATE"]}_'
-                      f'{sae_config["L1_COEFFICIENT"]}').lower()
-
-        if bool(es_client.indices.exists(index=index_name)):
-            search_indices.append(index_name)
-            search_indices_types.append("SAEIntervention")
-
     keywords = st.text_input(label="Keywords", placeholder="List keywords to search for in comma-separated style")
 
     dict_for_df = {
@@ -48,30 +35,17 @@ with Elasticsearch(
         "layer": [],
         "dim": [],
         "tokens": [],
-        "search_score": [],
-        "interpretability_score": [],
-        "interpretation": []
+        "search_score": []
     }
     for es_index, es_index_type in zip(search_indices, search_indices_types):
         result = es_client.search(index=es_index, q=keywords, size=config["top_k_tokens_for_ui"])["hits"]["hits"]
 
         for item in result:
-            if es_index_type == "LMDebuggerIntervention":
-                dict_for_df["index_type"].append(es_index_type)
-                dict_for_df["layer"].append(item["_source"]["layer"])
-                dict_for_df["dim"].append(item["_source"]["dim"])
-                dict_for_df["tokens"].append(item["_source"]["tokens"])
-                dict_for_df["search_score"].append(item["_score"])
-                dict_for_df["interpretability_score"].append(None)
-                dict_for_df["interpretation"].append(None)
-            elif es_index_type == "SAEIntervention":
-                dict_for_df["index_type"].append(es_index_type)
-                dict_for_df["layer"].append(item["_source"]["layer"])
-                dict_for_df["dim"].append(item["_source"]["dim"])
-                dict_for_df["tokens"].append(item["_source"]["tokens"])
-                dict_for_df["search_score"].append(item["_score"])
-                dict_for_df["interpretability_score"].append(item["_source"]["score"])
-                dict_for_df["interpretation"].append(item["_source"]["interpretation"])
+            dict_for_df["index_type"].append(es_index_type)
+            dict_for_df["layer"].append(item["_source"]["layer"])
+            dict_for_df["dim"].append(item["_source"]["dim"])
+            dict_for_df["tokens"].append(item["_source"]["tokens"])
+            dict_for_df["search_score"].append(item["_score"])
 
     if len(dict_for_df["search_score"]) > 0:
         pandas_df = pd.DataFrame(data=dict_for_df)
@@ -81,10 +55,8 @@ with Elasticsearch(
                 "index_type": "Type of Intervention Method",
                 "layer": "Layer Index",
                 "dim": "Feature Index",
-                "tokens": "Strongly activating Tokens",
-                "search_score": "Database-Hit strength",
-                "interpretability_score": "Interpretability Score",
-                "interpretation": st.column_config.Column(label="Interpretation", width=800)
+                "tokens": st.column_config.Column(label="Strongly activating Tokens", width=1200),
+                "search_score": "Database-Hit strength"
             },
             hide_index=True
         )
